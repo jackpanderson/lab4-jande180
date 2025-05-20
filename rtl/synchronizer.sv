81ddd3a2b50a56c08a4855c1797a6e0a4998b675
jackpanderson <52801396+jackpanderson@users.noreply.github.com>
2025-05-19 21:58:21 -0700

    Update synchronizer.sv

--- rtl/synchronizer.sv
+++ rtl/synchronizer.sv
@@ -1,41 +1,22 @@
-module synchronizer (input clk_r_i,
-                     input clk_w_i,
+module synchronizer (input clk_i,
                      input rst_i,
-                     input [3:0] grey_w_i,
-                     input [3:0] grey_r_i,
-                     output logic [3:0] grey_w_sync_o,
-                     output logic [3:0] grey_r_sync_o);
+                     input [3:0] grey_i,
+                     output logic [3:0] grey_sync_o);
     
-    logic [3:0] grey_w_delay, grey_r_delay;
+    logic [3:0] grey_delay;
 
-    always_ff @ (posedge clk_w_i, negedge rst_i)
+    always_ff @ (posedge clk_i, negedge rst_i)
     begin
         if (~rst_i)
         begin
-            grey_w_delay <=  'b0;
-            grey_w_sync_o <= 'b0;
+            grey_delay <=  'b0;
+            grey_sync_o <= 'b0;
         end
 
         else 
         begin
-            grey_w_delay <= grey_w_i;
-            grey_w_sync_o <= grey_w_delay;
+            grey_delay <= grey_i;
+            grey_sync_o <= grey_delay;
         end
     end
-
-    always_ff @ (posedge clk_r_i, negedge rst_i)
-    begin
-        if (~rst_i)
-        begin
-            grey_r_delay <=  'b0;
-            grey_r_sync_o <= 'b0;
-        end
-
-        else 
-        begin
-            grey_r_delay <= grey_r_i;
-            grey_r_sync_o <= grey_r_delay;
-        end
-    end
-    
 endmodule
